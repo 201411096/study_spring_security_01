964a1c0a0de6ebb48714bb019ff3a9bc91c8813c
201411096 <korea5781@gmail.com>
2024-12-07 18:59:24 +0900

    시큐리티 설정

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터 체인에 등록됨
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+//		super.configure(http);
+		
+		http.csrf().disable();
+		http.authorizeRequests()
+			.antMatchers("/user/**").authenticated()
+			.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.anyRequest().permitAll()
+			.and()
+			.formLogin()
+			.loginPage("/login");
+	}
+}
